ee6e89a376c2c562a77fcc4161a958565dc4e25f
ftovar8 <felix.godo@gmail.com>
2022-11-28 12:01:31 -0400

    Revert "Developing new packaging method"
    This reverts commit 54b166cf20f04646a4c3bfc94dd6430332b343ea.

--- bundle/instances.ts
+++
@@ -1,7 +0,0 @@
-import type {Bundle} from "./";
-
-export /*bundle*/ const instances = new class extends Map {
-    register(bundle: Bundle) {
-        this.set(bundle.vspecifier, bundle);
-    }
-}
